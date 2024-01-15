4f78ec5cfc9d34e4c31c7457862fce015f489d07
zhaolin <zhaolin@yzw.cn>
2024-01-15 18:05:49 +0800

    feat: 命令行模版工具

---
+++ bin/cli.js
@@ -0,0 +1,40 @@
+#!/usr/local/bin node
+
+const chalk = require("chalk");
+const semver = require("semver");
+const requiredVersion = require("../package.json").engines.node;
+
+function checkNodeVersion(wanted, id) {
+  if (!semver.satisfies(process.version, wanted)) {
+    console.log(
+      chalk.red(
+        "You are using Node " +
+          process.version +
+          ", but this version of " +
+          id +
+          " requires Node " +
+          wanted +
+          ".\nPlease upgrade your Node version."
+      )
+    );
+    process.exit(1);
+  }
+}
+
+checkNodeVersion(requiredVersion, "template-cli");
+
+const program = require("commander");
+program
+  .version(require("../package").version)
+  .option("-T, --template", "choose a template[react, ice]")
+  .usage("<command> [options]");
+
+program
+  .command("create")
+  .description("create one new template project")
+  .action((name, cmd) => {
+    console.log("创建模版项目");
+    require("../lib/create");
+  });
+
+program.parse(process.argv);
